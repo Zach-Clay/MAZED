f538dabdc121d420b39138f32004101e82b687ba
Zach Clay <zachclayc130@gmail.com>
2022-09-13 16:35:19 -0400

    init asp.net web api project

---
+++ api-backend/api-backend/Models/TestValues.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace api_backend.Models
+{
+    public class TestValues
+    {
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+    }
+}
+
