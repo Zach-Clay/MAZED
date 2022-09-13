fa2ab8cdbbbba17ea44e232cc4365ef2fe35dabe
evanbondura <ebondur@clemson.edu>
2022-09-13 15:11:56 -0400

    Removed Test Statement

--- ng-frontend/src/environments/environment.ts
+++ ng-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-const api_url = "https://localhost:____";
+const api_url = "https://localhost:7040/api/";
 
 /*
  * For easier debugging in development mode, you can import the following file
