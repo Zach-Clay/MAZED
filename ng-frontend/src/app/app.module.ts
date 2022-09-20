7e6146bf73f3c6ae0be0c791b750f91c8c879208
Zach Clay <zachclayc130@gmail.com>
2022-09-20 15:58:44 -0400

    added user service and environment url

--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
 
 //Angular Material
 import { MatButtonModule } from '@angular/material/button';
+
+//Components
 import { UserRegistrationComponent } from './pages/user-registration/user-registration.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component'
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
