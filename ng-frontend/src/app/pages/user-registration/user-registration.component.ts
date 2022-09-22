5613d643e7cbaa9741ce9c9367ddf55043877570
Danny <dannyandrewsaad@gmail.com>
2022-09-22 15:14:22 -0400

    style changes on registration page

--- ng-frontend/src/app/pages/user-registration/user-registration.component.ts
+++ ng-frontend/src/app/pages/user-registration/user-registration.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.css']
+  styleUrls: ['./user-registration.component.css'],
 })
 export class UserRegistrationComponent implements OnInit {
-
   public name: any = '';
   public username: any = '';
   public email: any = '';
@@ -14,14 +13,33 @@ export class UserRegistrationComponent implements OnInit {
   public repeat_password: any = '';
   public phone: any = '';
   public address: any = '';
+  public date: any = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit = () => {
-    console.log(this.name, this.username, this.email, this.password, this.repeat_password, this.phone, this.address);
-  }
+    if (this.password != this.repeat_password) {
+      alert('Passwords do not match');
+      return;
+    }
 
+    console.log(
+      this.name,
+      this.username,
+      this.email,
+      this.password,
+      this.repeat_password,
+      this.phone,
+      this.address
+    );
+    this.name = '';
+    this.username = '';
+    this.email = '';
+    this.password = '';
+    this.repeat_password = '';
+    this.phone = '';
+    this.address = '';
+  };
 }
