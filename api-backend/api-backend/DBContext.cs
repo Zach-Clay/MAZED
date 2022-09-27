e86ea3ee0cac37fd960c4278ba849b87914fe4b3
evanbondura <ebondur@clemson.edu>
2022-09-27 16:09:45 -0400

    Only works when i remove it out of project directory?

---
+++ api-backend/api-backend/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace api_backend
+{
+    public class DBContext
+    {
+        //Returns the connection string for the DB
+        public static string ConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+            IConfiguration config = builder.Build();
+
+            return config.GetValue<string>("ConnectionStrings:DB");
+        }//end ConnectionString
+    }
+}
---
+++ api-backend/api-backend/Program.cs
@@ -0,0 +1,59 @@
+﻿// app starting point
+
+using Microsoft.EntityFrameworkCore;
+using api_backend.Data;
+using MySqlConnector;
+using MazedDB.Data;
+using Newtonsoft.Json;
+using api_backend.Procedures;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//add db context service so our context can create our controller
+builder.Services.AddDbContext<MazedDBContext>(
+options =>
+{
+    //tell to use our string and versuon
+    options.UseMySql(builder.Configuration.GetConnectionString("DB"),
+    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.23-mysql"));
+});
+
+//adding the services to ignore referenceloop so we can fix error thrown for object disconnected
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+
+//service for our procedure just as we did for our original context
+//builder.Services.AddDbContext<MazedDBContextProcedures>(
+//options =>
+//{
+//    options.UseMySql(builder.Configuration.GetConnectionString("DB"),
+//    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.23-mysql"));
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
