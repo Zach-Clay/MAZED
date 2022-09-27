1fafee4fb185b0f3fa533b0c516480d544f39f23
Zach Clay <zachclayc130@gmail.com>
2022-09-27 16:28:30 -0400

    api runs

--- api-backend/api-backend/Controllers/UserController.cs
+++ api-backend/api-backend/Controllers/UserController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using api_backend.Data;
 using api_backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using api_backend.Logic;
