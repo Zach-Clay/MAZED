cf6350aca1fdb97ef57082f50b29ffc44fec706e
mwilson00 <43391533+mwilson00@users.noreply.github.com>
2022-10-07 20:45:39 -0400

    28: pointTrans controller added with API functions

---
+++ api-backend/api-backend/Controllers/PointTransController.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MazedDB.Data;
+using MazedDB.Models;
+
+namespace api_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PointTransController : ControllerBase
+    {
+        private readonly MazedDBContext _context;
+
+        public PointTransController(MazedDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PointTrans
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PointTransaction>>> GetPointTransactions()
+        {
+          if (_context.PointTransactions == null)
+          {
+              return NotFound();
+          }
+            return await _context.PointTransactions.ToListAsync();
+        }
+
+        // GET: api/PointTrans/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PointTransaction>> GetPointTransID(int id)
+        {
+            if (_context.Users == null) return NotFound();
+
+            var pointTrans = await _context.PointTransactions.Where(e => e.PointId == id).ToListAsync();
+
+            return pointTrans.ElementAt(0);
+        }
+
+        // PUT: api/PointTrans/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPointTransaction(int id, PointTransaction pointTransaction)
+        {
+            if (id != pointTransaction.PointId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pointTransaction).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PointTransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PointTrans
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PointTransaction>> PostPointTransaction(PointTransaction pointTransaction)
+        {
+          if (_context.PointTransactions == null)
+          {
+              return Problem("Entity set 'MazedDBContext.PointTransactions'  is null.");
+          }
+            _context.PointTransactions.Add(pointTransaction);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PointTransactionExists(pointTransaction.PointId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPointTransaction", new { id = pointTransaction.PointId }, pointTransaction);
+        }
+
+        // DELETE: api/PointTrans/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePointTransaction(int id)
+        {
+            if (_context.PointTransactions == null)
+            {
+                return NotFound();
+            }
+            var pointTransaction = await _context.PointTransactions.FindAsync(id);
+            if (pointTransaction == null)
+            {
+                return NotFound();
+            }
+
+            _context.PointTransactions.Remove(pointTransaction);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PointTransactionExists(int id)
+        {
+            return (_context.PointTransactions?.Any(e => e.PointId == id)).GetValueOrDefault();
+        }
+
+        //get point history for a user based on username
+
+        // [HttpGet("GetAllPointsForUser")]
+        // public async Task<PointTransaction?> GetAllPointsForUser(string username)
+        // {
+        //     return await _context.PointTransactions.GetUser().Where(p => p.UserId == id).FirstOrDefaultAsync();
+        // }
+    }
+}
