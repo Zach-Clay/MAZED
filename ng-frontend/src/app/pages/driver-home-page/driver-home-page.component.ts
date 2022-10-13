d6ad5e653533dfd28080e7d4e86dbacf510cbed8
Danny <dannyandrewsaad@gmail.com>
2022-10-13 14:05:57 -0400

    added a formatted point to dollar conversion

--- ng-frontend/src/app/pages/driver-home-page/driver-home-page.component.ts
+++ ng-frontend/src/app/pages/driver-home-page/driver-home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { CognitoService, UserInfo } from 'src/app/services/cognito.service';
-import { User, PointsChanges } from 'src/app/models/interfaces';
+import { User, PointsChanges, SponsorOrg } from 'src/app/models/interfaces';
 import { UserService } from 'src/app/services/user.service';
 import { PointsChangesService } from 'src/app/services/points-changes.service';
+import { SponsorOrgService } from 'src/app/services/sponsor-org.service';
 import { input } from '@aws-amplify/ui';
 
 @Component({
@@ -15,12 +16,15 @@ export class DriverHomePageComponent implements OnInit {
   @Input() cognitoUser: any;
   @Input() dbUser!: User;
   pointTransactions!: PointsChanges[];
+  p2drate!: string;
+  currentSponsor!: SponsorOrg;
 
   constructor(
     private router: Router,
     private cognitoService: CognitoService,
     private userService: UserService,
-    private pointsChangesService: PointsChangesService
+    private pointsChangesService: PointsChangesService,
+    private sponsorOrgService: SponsorOrgService
   ) {}
 
   ngOnInit(): void {
@@ -29,6 +33,13 @@ export class DriverHomePageComponent implements OnInit {
       .getTransactions(this.dbUser.id)
       .subscribe((pointsTrans) => {
         this.pointTransactions = pointsTrans;
+        this.sponsorOrgService
+          .getSponsorOrg(this.dbUser.sponsorId)
+          .subscribe((org) => {
+            this.currentSponsor = org;
+            this.p2drate = this.currentSponsor.dollarToPoint.toFixed(2);
+          });
+
         // display points now
       });
   }
--- ng-frontend/src/app/pages/sponsors-dashboard/sponsors-dashboard.component.ts
+++ ng-frontend/src/app/pages/sponsors-dashboard/sponsors-dashboard.component.ts
@@ -5,11 +5,12 @@ import { UserService } from 'src/app/services/user.service';
 import { SponsorOrgService } from 'src/app/services/sponsor-org.service';
 import { User, SponsorOrg, Application } from 'src/app/models/interfaces';
 import { ViewChild, ElementRef } from '@angular/core';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-sponsors-dashboard',
   templateUrl: './sponsors-dashboard.component.html',
-  styleUrls: ['./sponsors-dashboard.component.css']
+  styleUrls: ['./sponsors-dashboard.component.css'],
 })
 export class SponsorsDashboardComponent implements OnInit {
   user!: User;
@@ -26,16 +27,15 @@ export class SponsorsDashboardComponent implements OnInit {
     private cognitoService: CognitoService,
     private router: Router,
     private userService: UserService,
-    private sponsorOrgService: SponsorOrgService,
-  ) { 
-  }
+    private sponsorOrgService: SponsorOrgService
+  ) {}
 
   ngOnInit(): void {
     this.cognitoService
       .getUser()
       .then((user: any) => {
         //Get the user
-        this.userService.getUser(user.username).subscribe((data)=>{
+        this.userService.getUser(user.username).subscribe((data) => {
           this.user = data;
 
           //Get the orgs
@@ -43,31 +43,32 @@ export class SponsorsDashboardComponent implements OnInit {
             this.orgs = data;
 
             if (this.user.sponsorId !== 0) {
-              this.sponsorOrgService.getSponsorOrg(this.user.sponsorId).subscribe((o) => {
-                this.orgSelection = o;
-                //get drivers for that org
-                this.onSelectionChange();
-                this.showData = true;
-              })
-              
-            }
-            else {
+              this.sponsorOrgService
+                .getSponsorOrg(this.user.sponsorId)
+                .subscribe((o) => {
+                  this.orgSelection = o;
+                  //get drivers for that org
+                  this.onSelectionChange();
+                  this.showData = true;
+                });
+            } else {
               this.showData = true;
             }
           });
-
-        })
+        });
       })
       .catch((err) => {
         console.log(err);
-    });
+      });
   }
 
   onSelectionChange() {
     //update driver list
-    this.userService.getDriversBySponsor(this.orgSelection.id).subscribe((data) => {
-      this.drivers = data;
-    })
+    this.userService
+      .getDriversBySponsor(this.orgSelection.id)
+      .subscribe((data) => {
+        this.drivers = data;
+      });
   }
 
   toggleEdit() {
@@ -75,27 +76,33 @@ export class SponsorsDashboardComponent implements OnInit {
   }
 
   updateOrg() {
-    if (confirm("Are you sure you want to save? ")) {
-      let orgDesc = this.orgDescription.nativeElement.innerText
+    if (confirm('Are you sure you want to save? ')) {
+      let orgDesc = this.orgDescription.nativeElement.innerText;
 
       let dTP = this.dollarToPoint.nativeElement.innerText;
-      dTP = dTP.split("$").join("");
+      dTP = dTP.split('$').join('');
 
       this.orgSelection.orgDescription = orgDesc;
       this.orgSelection.dollarToPoint = dTP;
 
       //make api calls to update org info
-      this.sponsorOrgService.updateSponsorOrg(this.orgSelection.id, this.orgSelection);
+      this.sponsorOrgService.updateSponsorOrg(
+        this.orgSelection.id,
+        this.orgSelection
+      );
     }
   }
 
   removeDriver(driver: User) {
-    if (confirm("Are you sure you want to remove this driver? This action cannot be undone...")) {
+    if (
+      confirm(
+        'Are you sure you want to remove this driver? This action cannot be undone...'
+      )
+    ) {
       driver.sponsorId = 0;
       //make api calls to remove driver
       this.userService.updateUser(driver.id, driver);
-      this.drivers = this.drivers.filter(d => d.id !== driver.id);
+      this.drivers = this.drivers.filter((d) => d.id !== driver.id);
     }
   }
-
 }
