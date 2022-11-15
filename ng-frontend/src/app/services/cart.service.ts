655eca5a9b503fa01ccc13a46721bdd45c8397b3
Zach Clay <zachclayc130@gmail.com>
2022-11-15 10:55:08 -0500

    added cart service api calls

--- ng-frontend/src/app/services/cart.service.ts
+++ ng-frontend/src/app/services/cart.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { api_url, environment } from 'src/environments/environment';
+import { Cart } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getCartByUserId(userId: number) {
+    return this.http.get<Cart[]>(`${api_url}/cart/${userId}`);
+  }
+
+  public addToCart(cartItem: Cart) {
+    return this.http.post<Cart>(`${api_url}/cart`, cartItem);
+  }
+  
 }
